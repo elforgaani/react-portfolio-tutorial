a78a7b96f64047c5910d058442c2896a56041bee
elforgaani <mohamed.elforgani1999@gmail.com>
2024-02-22 20:54:50 +0200

    Add FooterCredits

--- src/components/Footer/components/FooterCredits.tsx
+++ src/components/Footer/components/FooterCredits.tsx
@@ -1,4 +1,4 @@
-const FooterCredits = () => {
+const FooterCredits = ():JSX.Element => {
     return (
         <div className="footer-credits py-2 text-center text-light" style={{ backgroundColor: '#1A252F' }}>
             <p>© 2021. All rights reserved</p>
