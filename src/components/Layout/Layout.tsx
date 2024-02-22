a5c2281c8e7137985e7ff33edfddfaceea6ad99a
elforgaani <mohamed.elforgani1999@gmail.com>
2024-02-22 20:55:14 +0200

    Add Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom"
 import Navbar from "../Navbar/Navbar"
 import Footer from "../Footer/Footer"
 
-const Layout = () => {
+const Layout = () :JSX.Element=> {
     return (
         <>
             <Navbar />
