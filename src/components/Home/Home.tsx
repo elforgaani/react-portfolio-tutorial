a89fd43c04ebbb2e892b7722b989a60df7e9f9af
elforgaani <mohamed.elforgani1999@gmail.com>
2024-02-22 20:54:57 +0200

    Add Home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,21 @@
-const Home = () => {
+
+import img from '../../assets/images/avatar.svg';
+import StarStylingCmp from '../StarStylingCmp/StarStylingCmp';
+
+const Home = (): JSX.Element => {
     return (
-        <section className="container">
-            <div className="home-section">
-                <img src="" alt="" />
-                <h2>icon</h2>
-                <p>Graphic Artist - Web Designer - Illustrator </p>
-            </div>
-        </section>
+        <div style={{ backgroundColor: '#1ABC9C' }} className='py-4'>
+            <section className="container">
+                <div className="home-section d-flex flex-column justify-content-center align-items-center text-light" >
+                    <img src={img} alt="Avatar" className='w-25' />
+                    <h2 className='text-uppercase my-3 fs-1 fw-bold'>Start Framework</h2>
+                    <StarStylingCmp />
+                    <p>Graphic Artist - Web Designer - Illustrator
+                    </p>
+                </div>
+            </section>
+        </div>
+
     )
 }
 export default Home;
