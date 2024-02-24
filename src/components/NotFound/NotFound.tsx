6b296637e0b7bc5a3400fbed84ccfc9d2c26ca3c
elforgaani <mohamed.elforgani1999@gmail.com>
2024-02-24 09:51:55 +0200

    Refactor Not found

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import img from '../../assets/images/pepe.png'
 
 export default function NotFound(): JSX.Element {
     return (
-        <div>NotFound</div>
+        <div className='d-flex flex-column justify-content-center align-items-center'>
+            <img src={img} className='w-50 my-3' alt="Not Found" />
+            <h2 className='fs-1 text-uppercase fw-bold py-2'> I handled Not Found, You can go back now!</h2>
+        </div>
     )
 }
